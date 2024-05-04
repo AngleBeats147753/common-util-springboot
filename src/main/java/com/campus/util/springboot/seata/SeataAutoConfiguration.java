659ba8341d3b0f323f5cbd963324d068aaba3b1f
黄磊 <huang147753@qq.com>
2024-05-04 19:29:11 +0800

    feat: 增加Seata的支持

---
+++ src/main/java/com/campus/util/springboot/seata/SeataAutoConfiguration.java
@@ -0,0 +1,51 @@
+package com.campus.util.springboot.seata;
+
+import com.campus.util.springboot.feign.EnableFeign;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 黄磊
+ */
+@Slf4j
+@Configuration
+@EnableFeign
+public class SeataAutoConfiguration implements WebMvcConfigurer {
+    @Bean
+    public RequestInterceptor seataIdRequestInterceptor() {
+        return new SeataIdRequestInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SeataIdRequestInterceptor1());
+    }
+
+    public static class SeataIdRequestInterceptor implements RequestInterceptor {
+        @Override
+        public void apply(RequestTemplate template) {
+            template.header(SeataIdUtil.HEADER_NAME, SeataIdUtil.getXid());
+        }
+    }
+
+    public static class SeataIdRequestInterceptor1 implements HandlerInterceptor {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+            String xid = SeataIdUtil.getXid(request);
+            if (xid == null) {
+                return true;
+            }
+            SeataIdUtil.bind(xid);
+            return true;
+        }
+    }
+}
---
+++ src/main/java/com/campus/util/springboot/seata/SeataIdUtil.java
@@ -0,0 +1,42 @@
+package com.campus.util.springboot.seata;
+
+import io.seata.core.context.RootContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * XId工具
+ *
+ * @author 黄磊
+ */
+public class SeataIdUtil {
+    public static final String HEADER_NAME = "XID";
+
+    /**
+     * 获取 XID
+     */
+    public static String getXid() {
+        return RootContext.getXID();
+    }
+
+    /**
+     * 从请求头中获取 XID
+     */
+    public static String getXid(HttpServletRequest request) {
+        return request.getHeader(HEADER_NAME);
+    }
+
+    /**
+     * 绑定 XID
+     */
+    public static void bind(String xid) {
+        RootContext.bind(xid);
+    }
+
+    /**
+     * 解绑 XID
+     */
+    public static void unbind() {
+        RootContext.unbind();
+    }
+}
